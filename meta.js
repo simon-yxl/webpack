d43a1222128b9582080ad701789e44e378b9d12b
yuanxiaolong <yuanxiaolong@58ganji.com>
2017-11-21 18:01:11 +0800

    新增配置

--- meta.js
+++ meta.js
@@ -18,17 +18,17 @@ module.exports = {
     "name": {
       "type": "string",
       "required": true,
-      "message": "Project name"
+      "message": "填写项目名称"
     },
     "description": {
       "type": "string",
       "required": false,
-      "message": "Project description",
-      "default": "A Vue.js project"
+      "message": "填写项目描述",
+      "default": "转转前端vue项目"
     },
     "author": {
       "type": "string",
-      "message": "Author"
+      "message": "作者"
     },
     "build": {
       "type": "list",
@@ -48,19 +48,146 @@ module.exports = {
     },
     "router": {
       "type": "confirm",
-      "message": "Install vue-router?"
+      "message": "是否使用vue-router?"
     },
     "vuex": {
       "type": "confirm",
-      "message": "Install vuex?"
+      "message": "是否使用vuex?"
     },
     "offline": {
       "type": "confirm",
-      "message": "Do you use offline package ?"
+      "message": "是否使用离线包功能？"
     },
     "babel-polyfill": {
       "type": "confirm",
-      "message": "Do you use babel-polyfill?"
+      "message": "是否安装babel-polyfill插件？"
+    },
+    "jenkins": {
+      "type": "confirm",
+      "message": "是否配置jenkins？"
+    },
+    "jenkins_test_cnd_address":{
+      "when": "jenkins",
+      "type": "list",
+      "message": "配置jenkins上静态资源存放的服务器地址",
+      "choices": [{
+        "name": "192.168.187.2",
+        "value": "192.168.187.2",
+        "short": "192.168.187.2"
+      }, {
+        "name": "192.168.187.16",
+        "value": "192.168.187.16",
+        "short": "192.168.187.16"
+      }, {
+        "name": "192.168.187.21",
+        "value": "192.168.187.21",
+        "short": "192.168.187.21"
+      }]
+    },
+    "jenkins_test_cnd_path":{
+      "when": "jenkins_test_cnd_address",
+      "type": "input",
+      "message": "填写测试环境cdn路径"
+    },
+    "jenkins_test_html_address":{
+      "when": "jenkins_test_cnd_path",
+      "type": "list",
+      "message": "配置jenkins上html文件存放的测试服务器地址",
+      "choices": [{
+        "name": "192.168.187.2",
+        "value": "192.168.187.2",
+        "short": "192.168.187.2"
+      }, {
+        "name": "192.168.187.16",
+        "value": "192.168.187.16",
+        "short": "192.168.187.16"
+      }, {
+        "name": "192.168.187.21",
+        "value": "192.168.187.21",
+        "short": "192.168.187.21"
+      }]
+    },
+    "jenkins_test_html_path":{
+      "when": "jenkins_test_html_address",
+      "type": "input",
+      "message": "填写测试环境html文件存放路径"
+    },
+    "jenkins_online_cdn_address":{
+      "when": "jenkins_test_html_path",
+      "type": "list",
+      "message": "配置jenkins上静态文件文件存放的线上服务器地址",
+      "choices": [{
+        "name": "img.58cdn.com.cn",
+        "value": "img.58cdn.com.cn",
+        "short": "img.58cdn.com.cn"
+      }, {
+        "name": "j1.58cdn.com.cn",
+        "value": "j1.58cdn.com.cn",
+        "short": "j1.58cdn.com.cn"
+      }, {
+        "name": "j2.58cdn.com.cn",
+        "value": "j2.58cdn.com.cn",
+        "short": "j2.58cdn.com.cn"
+      }, {
+        "name": "c.58cdn.com.cn",
+        "value": "c.58cdn.com.cn",
+        "short": "c.58cdn.com.cn"
+      }]
+    },
+    "jenkins_online_cnd_path":{
+      "when": "jenkins_online_cdn_address",
+      "type": "input",
+      "message": "填写线上环境cdn路径"
+    },
+    "jenkins_online_html_address":{
+      "when": "jenkins_online_cnd_path",
+      "type": "list",
+      "message": "配置jenkins上html文件存放的线上环境地址",
+      "choices": [{
+        "name": "m.zhuanzhuan.58.com",
+        "value": "m.zhuanzhuan.58.com",
+        "short": "m.zhuanzhuan.58.com"
+      }, {
+        "name": "m.zhuanzhuan.com",
+        "value": "m.zhuanzhuan.com",
+        "short": "m.zhuanzhuan.com"
+      }, {
+        "name": "m2.zhuanzhuan.com",
+        "value": "m2.zhuanzhuan.com",
+        "short": "m2.zhuanzhuan.com"
+      }]
+    },
+    "jenkins_online_html_path":{
+      "when": "jenkins_online_html_address",
+      "type": "input",
+      "message": "填写测试环境html文件存放线上环境路径"
+    },
+    "jenkins_npm_pkg":{
+      "when": "jenkins_online_html_path",
+      "type": "confirm",
+      "message": "如果使用其他转转内部sdk，需要在jenkins中配置（例如：@zz-vc/zui-swipe），是否需要配置？"
+    },
+    "jenkins_npm_pkg_list":{
+      "when": "jenkins_npm_pkg",
+      "type": "checkbox",
+      "message": "选择需要配置的 sdk",
+      "choices": [{
+        "name": "@zz-vc/zui-swipe",
+        "value": "@zz-vc/zui-swipe",
+        "short": "@zz-vc/zui-swipe"
+      }, {
+        "name": "@zz/api-cache",
+        "value": "@zz/api-cache",
+        "short": "@zz/api-cache"
+      }, {
+        "name": "@zz-vc/zui-imgdetailswipe",
+        "value": "@zz-vc/zui-imgdetailswipe",
+        "short": "@zz-vc/zui-imgdetailswipe"
+      }, {
+        "name": "@zz-vc/zui-imgdetailswipe",
+        "value": "@zz-vc/zui-imgdetailswipe",
+        "short": "@zz-vc/zui-imgdetailswipe"
+      }]
     },
     "unit": {
       "type": "confirm",
@@ -106,7 +233,9 @@ module.exports = {
     "src/router/**/*": "router",
     "src/vuex/**/*": "vuex",
     "config/offline.js": "offline",
-    "build/webpack.offline.conf.js": "offline"
+    "build/webpack.offline.conf.js": "offline",
+    "JENKINS_COMPLIER":"jenkins",
+    "jenkins_ftp_publish_servers.json":"jenkins"
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
